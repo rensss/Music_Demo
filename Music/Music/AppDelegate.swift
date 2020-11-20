cbfc64e3d2b1521505a087faa73c7b5ae0c5292d
Rzk <renzzz@qq.com>
2020-11-20 09:51:30 +0800

    feat init

---
+++ Music/Music/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Music
+//
+//  Created by Rzk on 2020/11/20.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
