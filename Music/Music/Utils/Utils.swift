c2f7a10bc26574688a98963237cdf2cca9e2e61e
Rzk <renzzz@qq.com>
2020-11-25 12:13:23 +0800

    fix duration 格式化显示

--- Music/Music/Utils/Utils.swift
+++ Music/Music/Utils/Utils.swift
@@ -10,7 +10,21 @@ import Foundation
 class Utils: NSObject {
     static func getFormatDuration(duration: Int) -> String {
         let min = duration/60
+        var minStr = ""
+        if min < 10 {
+            minStr = "0\(min)"
+        } else {
+            minStr = "\(min)"
+        }
+        
         let second = duration%60
-        return "\(min):\(second)"
+        var secondStr = ""
+        if second < 10 {
+            secondStr = "0\(second)"
+        } else {
+            secondStr = "\(second)"
+        }
+        
+        return minStr + ":" + secondStr
     }
 }
